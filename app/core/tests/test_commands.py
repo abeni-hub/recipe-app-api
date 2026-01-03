bc959c446330e1fb4032109fb0af148347478311
abeni <abeni@email.com>
2026-01-03 10:23:44 +0300

    command test with wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,3 +11,11 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
+@patch('app.core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Tests for 'wait_for_db' command."""
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available."""
+        
