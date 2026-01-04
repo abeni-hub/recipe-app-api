42781a7d8d1c6cb3d77b32057bd2e884e9478084
abeni <abeni@email.com>
2026-01-04 14:19:18 +0300

    adding test command for Command test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -21,3 +21,8 @@ class CommandTests(SimpleTestCase):
         patched_check.return_value = True
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default'])
+
+    def test_wait_for_db_delay(self, patched_check):
+        """Test waiting for db when db is not available."""
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
