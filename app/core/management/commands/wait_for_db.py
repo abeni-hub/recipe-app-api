abc098f5a24ad1d16d023544b9c2dbb8487732db
abeni <abeni@email.com>
2026-01-06 14:39:57 +0300

    Entry point

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,4 +15,15 @@ class Command(BaseCommand):
     """Django command to wait for the database to be available."""
 
     def handle(self, *args, **options):
-        pass
+        """ Entry point for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
