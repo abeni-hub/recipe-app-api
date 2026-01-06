69586cb7d5e3af8d7b974802fbe93dd921c9d862
abeni <abeni@email.com>
2026-01-06 14:50:32 +0300

    Database availabe run test working

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -12,7 +12,7 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('app.core.management.commands.wait_for_db.Command.check')
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Tests for 'wait_for_db' command."""
 
@@ -20,7 +20,7 @@ class CommandTests(SimpleTestCase):
         """Test waiting for db when db is available."""
         patched_check.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
         
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep , patched_check):
@@ -30,4 +30,4 @@ class CommandTests(SimpleTestCase):
         
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
