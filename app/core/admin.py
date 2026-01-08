63f242de3f39dba277598f945d471a323e348943
abeni <abeni@email.com>
2026-01-08 16:08:07 +0300

    the Last Fix

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+
+# Explicitly reference admin to satisfy flake8
+admin.site.site_header = 'Recipe API Admin'
--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class BaseModel(models.Model):
+    """Base model for future models."""
+
+    class Meta:
+        abstract = True
