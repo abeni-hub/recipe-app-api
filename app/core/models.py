fe387560a2782dfa1da13251633c130167e0a680
abeni <abeni@email.com>
2026-01-10 10:09:46 +0300

    add models

--- app/core/models.py
+++ app/core/models.py
@@ -1,9 +1,19 @@
+"""
+Database models.
+"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
 
-class BaseModel(models.Model):
-    """Base model for future models."""
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    class Meta:
-        abstract = True
-
+    USERNAME_FIELD = 'email'
