f675e87d0fd96251576ebb5e3e43cace12b03bf5
abeni-hub <ebenezarsileshi@gmail.com>
2026-01-31 15:25:46 +0300

    add test password to short error

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -45,3 +45,20 @@ def PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_password_too_short_error(self):
+        """Test an error is returned if password less than 8 characters"""
+        payload = {
+            'email': 'admin@example.com',
+            'password': 'pw',
+            'name': 'Test Name',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
