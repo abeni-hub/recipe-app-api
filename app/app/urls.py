9e3cfccebdaba14e09bce474072766fb07463543
abeni-hub <ebenezarsileshi@gmail.com>
2026-01-31 16:30:49 +0300

    add urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,25 +1,23 @@
-"""
-app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-"""
-
+from django.contrib import admin
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
     path(
-    'api/docs/',
+        'api/docs/',
         SpectacularSwaggerView.as_view(
             url_name='schema',
             title='API Documentation',
         ),
-    )
+        name='swagger-ui',
+    ),
+
+    path('api/user/', include('user.urls')),
 ]
