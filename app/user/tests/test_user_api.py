090087675d9dae15fb6d1627502d66ca916ab949
abeni-hub <ebenezarsileshi@gmail.com>
2026-02-01 19:33:56 +0300

    add test credate invalid credentials

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -82,3 +82,15 @@ def PublicUserApiTests(TestCase):
 
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test returns error if credentials are invalid"""
+        create_user(email='admin@example.com', password='goodpass123')
+
+        payload = {
+            'email': 'admin@example.com',
+            'password': 'wrongpass',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
